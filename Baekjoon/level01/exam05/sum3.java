697be2cec6de192ecd874f85440b747ecf9c9e9f
HyeonWoo Jo <johw2723@gmail.com>
2021-08-30 11:11:06 +0900

    Reader + Writer

---
+++ Baekjoon/level01/exam05/sum3.java
@@ -0,0 +1,26 @@
+package level01.exam05;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class sum3 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		String result = Integer.toString(a + b);		
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(result);
+		bw.flush();
+		bw.close();
+	}
+
+}
