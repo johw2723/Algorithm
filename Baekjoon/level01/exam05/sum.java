d1243a10ccde7811c7db256482b7e9b551eecd06
HyeonWoo Jo <johw2723@gmail.com>
2021-08-30 10:53:52 +0900

    단순 Scanner 사용

---
+++ Baekjoon/level01/exam05/sum.java
@@ -0,0 +1,15 @@
+package level01.exam05;
+
+import java.util.Scanner;
+
+public class sum {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		System.out.println(a + b);
+	}
+
+}
