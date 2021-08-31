01c0200c211af613dfed86ca33a43ed96e36e413
HyeonWoo Jo <johw2723@gmail.com>
2021-08-31 10:01:25 +0900

    Reader + Writer
    88ms

---
+++ Baekjoon/level01/exam07/multiply4.java
@@ -0,0 +1,30 @@
+package level01.exam07;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class multiply4 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		String result = Integer.toString(a * b);
+		
+		br.close();
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(result);
+		
+		bw.flush();
+		bw.close();
+	}
+	
+}
