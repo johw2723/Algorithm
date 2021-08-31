fb3365df3e7c5245cb35cad89216fe134752e1e6
HyeonWoo Jo <johw2723@gmail.com>
2021-08-31 09:56:38 +0900

    scanner

---
+++ Baekjoon/level01/exam07/multiply.java
@@ -0,0 +1,16 @@
+package level01.exam07;
+
+import java.util.Scanner;
+
+public class multiply {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		scanner.close();	
+		
+		System.out.println(a * b);
+	}
+	
+}
