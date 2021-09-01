32a0e2809f8be0345de1f9380d69abf2c1987121
HyeonWoo Jo <johw2723@gmail.com>
2021-09-01 13:28:23 +0900

    BufferedReader

---
+++ Baekjoon/level01/exam09/calculation2.java
@@ -0,0 +1,23 @@
+package level01.exam09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class calculation2 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+	}
+}
