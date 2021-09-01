23b8a8f86fbbaf82ee630a1709239bc10461c0ba
HyeonWoo Jo <johw2723@gmail.com>
2021-09-01 13:47:12 +0900

    Scanner 120ms

---
+++ Baekjoon/level01/exam10/remainder.java
@@ -0,0 +1,23 @@
+package level01.exam10;
+
+import java.util.Scanner;
+
+public class remainder {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	       
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        sc.close();
+        
+        System.out.println((a+b)%c);
+        System.out.println((a%c + b%c)%c);
+        System.out.println((a*b)%c);
+        System.out.println((a%c * b%c)%c);
+
+	}
+
+}
