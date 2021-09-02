abba58a1a8dbb32d2a07d88ebd3b73024ce80768
HyeonWoo Jo <johw2723@gmail.com>
2021-09-02 12:21:50 +0900

    Scanner 124ms

---
+++ Baekjoon/level01/exam11/threeDigits.java
@@ -0,0 +1,21 @@
+package level01.exam11;
+
+import java.util.Scanner;
+
+public class threeDigits {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		String b = scanner.next();
+		
+		scanner.close();
+		
+		System.out.println(a * (b.charAt(2) - '0'));
+		System.out.println(a * (b.charAt(1) - '0'));
+		System.out.println(a * (b.charAt(0) - '0'));
+		System.out.println(a * Integer.parseInt(b));
+	}
+
+}
