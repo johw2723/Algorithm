2d2b48eb462b1502291927cec15c0059d6c2d782
HyeonWoo Jo <johw2723@gmail.com>
2021-09-03 10:28:30 +0900

    BufferedReader 92ms

---
+++ Baekjoon/level02/exam01/comparison3.java
@@ -0,0 +1,22 @@
+package level02.exam01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class comparison3 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		br.close();
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		System.out.println((A>B) ? ">" : ((A<B) ? "<" : "=="));
+	}
+
+}
