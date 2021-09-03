761f21255d7dc9d33738ff1559cae35a270d4a9e
HyeonWoo Jo <johw2723@gmail.com>
2021-09-03 10:27:32 +0900

    Scanner 112ms

---
+++ Baekjoon/level02/exam01/comparison.java
@@ -0,0 +1,25 @@
+package level02.exam01;
+
+import java.util.Scanner;
+
+public class comparison {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		sc.close();
+		
+		if(A > B) {
+			System.out.println('>');
+		} else if (A < B) {
+			System.out.println('<');
+		} else {
+			System.out.println("==");
+		}
+
+	}
+
+}
