0ea5a2adf747322474e29813683c87a6ea6f541e
HyeonWoo Jo <johw2723@gmail.com>
2021-09-06 12:17:13 +0900

    Scanner 112ms

---
+++ Baekjoon/level02/exam03/leapYear.java
@@ -0,0 +1,24 @@
+package level02.exam03;
+
+import java.util.Scanner;
+
+public class leapYear {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		sc.close();
+		
+		if(year%4==0) {
+			if(year%100!=0 || year%400==0) {
+				System.out.println('1');
+			} else {
+				System.out.println('0');
+			}
+		} else {
+			System.out.println('0');
+		}
+
+	}
+
+}
