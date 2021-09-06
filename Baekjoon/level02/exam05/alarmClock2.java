98930717893e531e12de707700e88338048dedc8
HyeonWoo Jo <johw2723@gmail.com>
2021-09-06 12:50:46 +0900

    BufferedReader 76ms

---
+++ Baekjoon/level02/exam05/alarmClock2.java
@@ -0,0 +1,32 @@
+package level02.exam05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class alarmClock2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		br.close();
+		
+		int h = Integer.parseInt(st.nextToken()); // ½Ã
+		int m = Integer.parseInt(st.nextToken()); // ºÐ
+		
+		if(m<45) {
+			h--;
+			m = 60 - (45-m);
+			if(h < 0) {
+				h = 23;
+			}
+			System.out.println(h + " " + m);
+		} else {
+			System.out.println(h + " " + (m - 45));
+		}
+
+	}
+
+}
