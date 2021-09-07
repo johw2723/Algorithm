c0be6fea3083c80c438875946deac92e701d8baf
HyeonWoo Jo <johw2723@gmail.com>
2021-09-07 11:03:17 +0900

    BufferedReader 76ms

---
+++ Baekjoon/level02/exam06/mulTable2.java
@@ -0,0 +1,20 @@
+package level02.exam06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class mulTable2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		br.close();
+		
+		for(int i=1; i<=9; i++) {
+			System.out.println(num + " * " + i + " = " + num*i);
+		}
+
+	}
+
+}
