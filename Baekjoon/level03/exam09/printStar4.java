6eaa0bc01da0ca852816f6454e9cdc5a3d9c8966
HyeonWoo Jo <johw2723@gmail.com>
2021-09-13 11:52:21 +0900

    BufferedReader + BufferedWriter 80ms

---
+++ Baekjoon/level03/exam09/printStar4.java
@@ -0,0 +1,30 @@
+package level03.exam09;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class printStar4 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=i; j++) {
+				bw.write('*');
+			}
+			bw.write('\n');
+		}
+		
+		bw.flush();
+		bw.close();
+
+	}
+
+}
