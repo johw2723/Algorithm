a1c3c350428d647ec5b24359e1c851479e8efe02
HyeonWoo Jo <johw2723@gmail.com>
2021-09-13 12:19:41 +0900

    BufferedReader + StringBuilder 80ms

---
+++ Baekjoon/level03/exam10/printStarSort3.java
@@ -0,0 +1,31 @@
+package level03.exam10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class printStarSort3 {
+	
+	//√‚¿˙ : https://www.acmicpc.net/problem/2439
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N-i; j++) {
+				sb.append(" ");
+			}
+			for(int k=1; k<=i; k++) {
+				sb.append('*');
+			}
+			sb.append('\n');
+		}
+		System.out.println(sb);
+
+	}
+
+}
