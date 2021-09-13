824a77d08d02c71758749cc28b6d3e13c6df182e
HyeonWoo Jo <johw2723@gmail.com>
2021-09-13 12:19:14 +0900

    Scanner 272ms

---
+++ Baekjoon/level03/exam10/printStarSort.java
@@ -0,0 +1,26 @@
+package level03.exam10;
+
+import java.util.Scanner;
+
+public class printStarSort {
+	
+	//√‚¿˙ : https://www.acmicpc.net/problem/2439
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=i; k++) {
+				System.out.print('*');
+			}
+			System.out.print('\n');
+		}
+
+	}
+
+}
