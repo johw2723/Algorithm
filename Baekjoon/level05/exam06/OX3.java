6918116c1db0f59102a762f33a7a3edf05900217
HyeonWoo Jo <johw2723@gmail.com>
2021-09-23 12:16:44 +0900

    BufferdReader + StringBuilder 88ms

---
+++ Baekjoon/level05/exam06/OX3.java
@@ -0,0 +1,42 @@
+package level05.exam06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class OX3 {
+	
+	// 출저 : https://www.acmicpc.net/problem/8958
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = new String[Integer.parseInt(br.readLine())];
+		
+		// 배열에 테스트케이스 넣기
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = br.readLine();
+		}
+		br.close();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<arr.length; i++) {
+			int cnt = 0; // 연속횟수
+			int sum = 0; // 누적합
+			
+			for(int j=0; j<arr[i].length(); j++) {
+				if(arr[i].charAt(j) == 'O') {
+					cnt++;
+				} else {
+					cnt = 0;
+				}
+				sum += cnt;
+			}
+			sb.append(sum).append("\n");
+		}
+		
+		System.out.println(sb);
+
+	}
+
+}
