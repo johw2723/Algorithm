1ea238fb31583377fd21a10ba1b406429fb5ef87
HyeonWoo Jo <johw2723@gmail.com>
2021-09-27 12:03:42 +0900

    124ms

---
+++ Baekjoon/level06/exam02/selfNumber.java
@@ -0,0 +1,37 @@
+package level06.exam02;
+
+public class selfNumber {
+	
+	// 출저 : https://www.acmicpc.net/problem/4673
+
+	public static void main(String[] args) {
+		
+		boolean[] check = new boolean[10001];
+		
+		for(int i=1; i<10001; i++) { // 문제에서 요구한 범위
+			int n = d(i);
+			
+			if(n < 10001) {
+				check[n] = true;
+			}
+		}
+		
+		for(int i=1; i<10001; i++) {
+			if(!check[i]) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+	private static int d(int number) {
+		int sum = number;
+		
+		while(number != 0) {
+			sum = sum + (number % 10); // 주어진 값의 첫 째 자리수
+			number = number/10; // 10을 나누어 첫 째 자리를 없앤다.
+		}
+		return sum;
+	}
+
+}
