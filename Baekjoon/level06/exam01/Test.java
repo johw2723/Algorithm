d6368c76b3dca052cbc82e7e087fe61d95c008d5
HyeonWoo Jo <johw2723@gmail.com>
2021-09-27 11:49:45 +0900

    24ms

---
+++ Baekjoon/level06/exam01/Test.java
@@ -0,0 +1,13 @@
+package level06.exam01;
+
+public class Test {
+	long sum(int[] a) {
+		long ans = 0;
+		
+		for(int i=0; i<a.length; i++) {
+			ans += a[i];
+		}
+		
+		return ans;
+	}
+}
---
+++ Baekjoon/level06/exam01/integerNSum.java
@@ -0,0 +1,24 @@
+package level06.exam01;
+
+import java.util.Scanner;
+
+public class integerNSum {
+	
+	// √‚¿˙ : https://www.acmicpc.net/problem/15596
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[sc.nextInt()];
+		
+		for(int i=0; i<arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		sc.close();
+		
+		long result = new Test().sum(arr);
+		
+		System.out.println(result);
+
+	}
+
+}
