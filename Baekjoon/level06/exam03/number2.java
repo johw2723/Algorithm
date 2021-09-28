71f58f70eb2984517ffb7fe1a99516a9599186cd
HyeonWoo Jo <johw2723@gmail.com>
2021-09-28 13:27:43 +0900

    BufferedReader 84ms

---
+++ Baekjoon/level06/exam03/number2.java
@@ -0,0 +1,48 @@
+package level06.exam03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class number2 {
+	
+	// 출저 : https://www.acmicpc.net/problem/1065
+	
+	public static void main(String[] args) throws IOException {
+		
+		// 1~99 : 등차수열 (1 ~ 9 : 그 자체로 수열 / 10 ~ 99 각 자리의 차가 공차)
+		// 100~1000 : 실제 구해야할 값의 범위
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print(checkSequence(Integer.parseInt(br.readLine())));
+		br.close();
+		
+		
+	}
+	
+	public static int checkSequence(int num) {
+		int cnt = 0;
+		
+		if(num<100) {
+			return num;
+		} else {
+			cnt = 99;
+			if(num == 1000) {
+				num = 999; // 예외처리 : 최대값인 1000은 등차수열이 아니기 때문에 변경
+			}
+			
+			for(int i = 100; i<=num; i++) {
+				int hun = i/100;
+				int ten = (i/10)%10;
+				int one = i%10;
+				
+				if((hun - ten) == (ten - one)) {
+					cnt++;
+				}
+			}
+		}
+		
+		return cnt;
+	}
+
+}
