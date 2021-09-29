69760b4cc818fbb56953b811ffa85d9336652fa5
HyeonWoo Jo <johw2723@gmail.com>
2021-09-29 12:59:24 +0900

    System.in 76ms

---
+++ Baekjoon/level07/exam01/ASCII4.java
@@ -0,0 +1,15 @@
+package level07.exam01;
+
+import java.io.IOException;
+
+public class ASCII4 {
+	
+	// √‚¿˙ : https://www.acmicpc.net/problem/11654
+
+	public static void main(String[] args) throws IOException {
+		int ascii = System.in.read();
+		System.out.println(ascii);
+
+	}
+
+}
