713a00e608fadb36fa4433caf9e1494448a20949
HyeonWoo Jo <johw2723@gmail.com>
2021-09-29 12:56:29 +0900

    Scanner2 108ms

---
+++ Baekjoon/level07/exam01/ASCII2.java
@@ -0,0 +1,17 @@
+package level07.exam01;
+
+import java.util.Scanner;
+
+public class ASCII2 {
+	
+	// √‚¿˙ : https://www.acmicpc.net/problem/11654
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int ascii = sc.next().charAt(0);
+		sc.close();
+		System.out.println(ascii);
+
+	}
+
+}
