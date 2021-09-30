1d9680d106b7703b0ebe3420715dfe4d75177b98
HyeonWoo Jo <johw2723@gmail.com>
2021-09-30 11:09:24 +0900

    BufferedReader 76ms

---
+++ Baekjoon/level07/exam03/search2.java
@@ -0,0 +1,70 @@
+package level07.exam03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class search2 {
+	
+	// √‚¿˙ : https://www.acmicpc.net/problem/10809
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String S = br.readLine();
+		br.close();
+		
+		System.out.print(S.indexOf('a'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('b'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('c'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('d'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('e'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('f'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('g'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('h'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('i'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('j'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('k'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('l'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('m'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('n'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('o'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('p'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('q'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('r'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('s'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('t'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('u'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('v'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('w'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('x'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('y'));
+		System.out.print(" ");
+		System.out.print(S.indexOf('z'));
+		
+	}
+
+}
