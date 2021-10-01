cd050c1560d413bcfff54e7d283a7d464663b8af
HyeonWoo Jo <johw2723@gmail.com>
2021-10-01 11:06:09 +0900

    Scanner 396ms

---
+++ Baekjoon/level07/exam06/countWords.java
@@ -0,0 +1,33 @@
+package level07.exam06;
+
+import java.util.Scanner;
+
+public class countWords {
+	
+	// 출저 : https://www.acmicpc.net/problem/1152
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine().trim(); // trim() :  문자열 앞 뒤 공백을 빈 문자열로 변환, 중간에 있는 공백은 해당 안됨.
+		sc.close();
+		
+		// String str = "" != String str = null
+		// 빈문자열과 할당 없음은 같지 않다.
+		
+		int cnt = 0;
+		String[] token = str.split(" ");
+		
+		if(str.length() == 0) { // 공백으로만 주어진 문자열인지 확인
+			System.out.print(cnt);		
+		} else {				
+			for(int i=0; i<token.length; i++) {
+				if(token[i] != "") {
+					cnt++;
+				}
+			}
+			System.out.print(cnt);
+		}
+
+	}
+
+}
