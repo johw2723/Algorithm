18c1a9c85e705b7494104fa559aeca762bd56f30
HyeonWoo Jo <johw2723@gmail.com>
2021-10-13 11:22:48 +0900

    BufferedReader 80ms

---
+++ Baekjoon/level09/exam02/primeNumber2.java
@@ -0,0 +1,56 @@
+package level09.exam02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class primeNumber2 {
+
+	// 출저 : https://www.acmicpc.net/problem/2581
+	public static boolean prime[];
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int M = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		prime = new boolean[N + 1]; // 배열 생성
+		get_prime();
+		
+		// 소수 합 및 최솟값
+		int sum = 0;
+		int min = Integer.MAX_VALUE;
+		for(int i=M; i<=N; i++) {
+			if(prime[i] == false) { // 소수일 때
+				sum += i;
+				if(min == Integer.MAX_VALUE) { // 첫 소수가 최솟값
+					min = i;
+				}
+			}
+		}
+		
+		if(sum == 0) { // 소수가 없다면
+			System.out.println(-1);
+		}
+		else {
+			System.out.println(sum);
+			System.out.println(min);
+		}
+
+	}
+
+	// 소수구하기 : 에라토스테네스 체 알고리즘 
+	private static void get_prime() {
+		prime[0] = true;
+		prime[1] = true;
+		
+		for(int i=2; i<=Math.sqrt(prime.length); i++) {
+			for(int j=i*i; j<prime.length; j+=i) {
+				prime[j] = true;
+			}
+		}
+		
+	}
+
+}
