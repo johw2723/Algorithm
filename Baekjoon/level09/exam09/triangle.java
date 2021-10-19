9e9eff717c265e10231192ca5643ab6e114413b0
HyeonWoo Jo <johw2723@gmail.com>
2021-10-19 11:26:01 +0900

    Scanner 116ms

---
+++ Baekjoon/level09/exam09/triangle.java
@@ -0,0 +1,39 @@
+package level09.exam09;
+
+import java.util.Scanner;
+
+public class triangle {
+	
+	// √‚¿˙ : https://www.acmicpc.net/problem/4153
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			
+			if(a == 0 && b == 0 && c == 0) {
+				break;
+			}
+			
+			if(a*a == (b*b) + (c*c)) {
+				System.out.println("right");
+			}
+			else if(b*b == (a*a) + (c*c)) {
+				System.out.println("right");
+			} 
+			else if(c*c == (a*a) + (b*b)) {
+				System.out.println("right");
+			}
+			else {
+				System.out.println("wrong");
+			}
+		}
+		
+		sc.close();
+
+	}
+
+}
