c940353ad5922dd0db2b00c2491647cef45ad115
HyeonWoo Jo <johw2723@gmail.com>
2021-10-21 13:03:30 +0900

    BufferedReader + StringBuilder 252ms

---
+++ Baekjoon/level10/exam03/star2.java
@@ -0,0 +1,66 @@
+package level10.exam03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class star2 {
+	
+	// 출저 : https://www.acmicpc.net/problem/2447
+	
+	static char[][] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // N은 3의 거듭제곱
+		br.close();
+		
+		arr = new char[N][N];
+		star(0, 0, N, false); // x, y, N, boolean
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				sb.append(arr[i][j]);
+			}
+			sb.append('\n');
+		}
+		System.out.print(sb);
+	}
+
+	// blank가 true 라면 공백을 의미한다.
+	private static void star(int x, int y, int N, boolean blank) {
+		// 공백칸일 경우
+		if(blank) {
+			for(int i=x; i<x+N; i++) {
+				for(int j=y; j<y+N; j++) {
+					arr[i][j] = ' ';
+				}
+			}
+			return;
+		}
+		
+		// 더이상 쪼갤 수 없는 블록일 때
+		if(N == 1) {
+			arr[x][y] = '*';
+			return;
+		}
+		
+		// N = 27 일 경우 한 블록의 사이즈는 9이고, N = 9 일 경우 한 블록의 사이즈는 3이다	
+		int size = N / 3; // 해당 블록의 한 칸을 담을 변수
+		int count = 0; // 별 출력 누적
+		
+		for(int i=x; i<x+N; i+=size) {
+			for(int j=y; j<y+N; j+=size) {
+				count++;
+				if(count == 5) { // 공백 칸일 경우
+					star(i, j, size, true);
+				}
+				else {
+					star(i, j, size, false);
+				}
+			}
+		}
+	}
+
+}
