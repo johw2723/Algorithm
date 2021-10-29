1811c279cdd7fd9b3fac6021373a8888acd5c611
HyeonWoo Jo <johw2723@gmail.com>
2021-10-29 11:36:14 +0900

    BufferedReader + 경우의 수 좁히기 80ms

---
+++ Baekjoon/level11/exam05/movie4.java
@@ -0,0 +1,101 @@
+package level11.exam05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class movie4 {
+	
+	// 출저 : https://www.acmicpc.net/problem/1436
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+		
+		if(N>1) {
+			naming(N);
+		}
+		else {
+			System.out.print(666);
+		}
+
+	}
+
+	private static void naming(int n) {
+		int count = 1;
+		int prev_digit = 0; // 선수 자릿수
+		int num = 0;        // 선수 자릿수를 제외한 나머지 뒷 자리수
+		
+		// prev_digit이 X...666X 이면서 X...6666 이 아닐경우 (ex : 6660, 6661, ... )
+		while(true) {
+			if(((prev_digit % 10000) / 10) == 666 && prev_digit % 10 != 6) {
+				for(int i=0; i<1000; i++) {
+					if(count == n) {
+						System.out.print(prev_digit * 1000 + num);
+						return;
+					}
+					num++;
+					count++;
+				}
+				prev_digit++;
+			}
+			
+			// prev_digit이 X...666 일 경우 (ex : 666, 1666, ...)
+			else if(prev_digit % 1000 == 666) {
+				num = 0;
+				for(int i=0; i<1000; i++) {
+					if(count == n) {
+						System.out.print(prev_digit * 1000 + num);
+						return;
+					}
+					num++;
+					count++;				
+				}
+				prev_digit++;
+			}
+			
+			//prev_digit이 X...6 일 경우 (ex : 66_600, 166_600, ...)
+			else if(prev_digit % 100 == 66) {
+				num = 600;
+				for(int i=0; i<100; i++) {
+					if(count == n) {
+						System.out.print(prev_digit * 1000 + num);
+						return;
+					}
+					count++;
+					num++;
+				}
+				prev_digit++;
+			}
+			
+			// prev_digit이 X...6 일 경우 (ex : 6_660, 16_660, ...)
+			else if(prev_digit % 10 == 6) {
+				num = 660;
+				for(int i=0; i<10; i++) {
+					if(count == n) {
+						System.out.print(prev_digit * 1000 + num);
+						return;
+					}
+					num++;
+					count++;
+				}
+				prev_digit++;
+			}
+			
+			// 그 외 (ex : x_666)
+			else {
+				num = 666;
+				if(count == n) {
+					System.out.print(prev_digit * 1000 + num);
+					return;
+				}
+				count++;
+				prev_digit++;
+			}
+			
+		}
+		
+	}
+
+}
