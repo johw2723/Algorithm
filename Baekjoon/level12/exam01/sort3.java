7dcbfd1daf8a95bc5b587ac0751d364151adf5cf
HyeonWoo Jo <johw2723@gmail.com>
2021-11-01 17:23:23 +0900

    BufferedReader + StringBuilder 버블정렬 100ms

---
+++ Baekjoon/level12/exam01/sort3.java
@@ -0,0 +1,45 @@
+package level12.exam01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class sort3 {
+	
+	// 출저 : https://www.acmicpc.net/problem/2750
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+		
+		br.close();
+		
+		for(int i=0; i<N-1; i++) {
+			for(int j=i+1; j<N; j++) {
+				
+				if(arr[i] > arr[j]) {
+					// 값 교환
+					int temp = arr[j];
+					arr[j] = arr[i]; 
+					arr[i] = temp;
+				}
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int value : arr) {
+			sb.append(value).append('\n');
+		}
+		
+		System.out.print(sb);
+
+	}
+
+}
