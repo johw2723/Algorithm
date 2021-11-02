86a1d9041fb71d221b31615e4b45cd93375bafc2
HyeonWoo Jo <johw2723@gmail.com>
2021-11-02 12:10:25 +0900

    Scanner 시간초과

---
+++ Baekjoon/level12/exam02/sort.java
@@ -0,0 +1,37 @@
+package level12.exam02;
+
+import java.util.Scanner;
+
+public class sort {
+	
+	// √‚¿˙ : https://www.acmicpc.net/problem/2751
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		int[] arr = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		sc.close();
+		
+		for(int i=0; i<N-1; i++) {
+			for(int j=i+1; j<N; j++) {
+				if(arr[i] > arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		
+		for(int value : arr) {
+			System.out.println(value);
+		}
+
+	}
+
+}
