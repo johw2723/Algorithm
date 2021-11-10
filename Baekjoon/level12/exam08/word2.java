7c431ea2b0fb81d036ce90fceea6bf6d2e61ffba
HyeonWoo Jo <johw2723@gmail.com>
2021-11-10 12:25:39 +0900

    Scanner + StringBuilder 516ms

---
+++ Baekjoon/level12/exam08/word2.java
@@ -0,0 +1,51 @@
+package level12.exam08;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class word2 {
+	
+	// 출저 : https://www.acmicpc.net/problem/1181
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		String[] arr = new String[N];
+		
+		sc.nextLine();
+		
+		for(int i=0; i<N; i++) {
+			arr[i] = sc.nextLine();
+		}
+		
+		sc.close();
+		
+		Arrays.sort(arr, new Comparator<String>() {
+			public int compare(String s1, String s2) {
+				// 단어 길이가 같을 경우
+				if(s1.length() == s2.length()) {
+					return s1.compareTo(s2);
+				}
+				// 그 외의 경우
+				else {
+					return s1.length() - s2.length();
+				}
+			}
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(arr[0]).append('\n');
+		
+		for(int i=1; i<N; i++) {
+			// 중복되지 않는 단어만 출력
+			if(!arr[i].equals(arr[i-1])) {
+				sb.append(arr[i]).append('\n');
+			}
+		}
+		
+		System.out.print(sb);
+
+	}
+
+}
